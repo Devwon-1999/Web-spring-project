b888c0fdbbfbcc9501dd8aec09f18590f42a9af6
Devwon-1999 <Devwon.1999@gmail.com>
2024-04-21 08:38:28 +0900

    update

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter @Setter
+public class Category {
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item", // 중간테이블이 있어야하기 때문이다
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns =  @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+    //연관관계 편의 매서드
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
+}
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -16,15 +18,34 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne // 다 : 1 연관관계 어노테이션
+    @ManyToOne(fetch = LAZY) // 다 : 1 연관관계 어노테이션
     @JoinColumn(name = "member_id") //매핑을 member_id로 한다. 외래키의 이름이 member_id
     private Member member;
 
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id") // 주문에서 배송을 조회할 경우가 많기때문에 주문이 주가된다.
     private Delivery delivery;
 
     private LocalDateTime orderDate; //hibernate가 자동으로 지원해주기때문에 시간관련 어노테이션이 필요가 없다
 
+    @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태
+
+    //연관관계 편의 매서드
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
