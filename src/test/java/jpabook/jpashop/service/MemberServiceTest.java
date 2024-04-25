ab823faa11332a9f465e4f538ae8e7735c310543
Devwon-1999 <Devwon.1999@gmail.com>
2024-04-26 07:52:03 +0900

    sign in test

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,7 +1,10 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,13 +16,19 @@ import static org.junit.Assert.*;
 @Transactional
 public class MemberServiceTest {
 
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
     @Test
     public void 회원가입() throws Exception{
         //given
-
+        Member member = new Member();
+        member.setName("kim");
         //when
+        Long saveId = memberService.join(member);
 
         //then
+        assertEquals(member, memberRepository.findOne(saveId));
     }
 
     @Test
