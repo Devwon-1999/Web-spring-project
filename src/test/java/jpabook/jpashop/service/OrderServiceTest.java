a9a77c1c83c7966a1ec0e739c5c89429b114fddf
Devwon-1999 <Devwon.1999@gmail.com>
2024-06-12 06:08:04 +0900

    OrderServiceTest - 상품주문(주문취소)

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -59,6 +59,23 @@ public class OrderServiceTest {
         fail("재고 수량 부족 예외가 발생해야 한다.");
     }
 
+    @Test
+    public void 주문취소() throws Exception{
+        //given
+        Member member = createMember();
+        Book item = createBook("시골 JPA", 10000, 10);
+
+        int orderCount = 2;
+        Long orderId = orderService.order(member.getId(), item.getId(), orderCount);
+        //when
+        orderService.cancelOrder(orderId);
+
+        //then
+        Order getOrder = orderRepository.findOne(orderId);
+        assertEquals("주문 취소시 상태는 CANCEL이다.", OrderStatus.CANCEL, getOrder.getStatus());
+        assertEquals("주문이 취소된 상품은 그만큼 재고가 증가해야 한다.", 10, item.getStockQuanitiy());
+    }
+
     private Book createBook(String name, int price, int stockQuanitiy) {
         Book book = new Book();
         book.setName(name);
