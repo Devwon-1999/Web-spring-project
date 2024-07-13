5197e2fb20343d691e41ff39c78beb63762ab873
Devwon-1999 <Devwon.1999@gmail.com>
2024-07-14 02:31:36 +0900

    orderForm update

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -37,7 +37,7 @@ public class OrderController {
     public String order (@RequestParam("memberId") Long memberId,
                          @RequestParam("itemId") Long itemId,
                          @RequestParam("count") int count){
-
+        orderService.order(memberId, itemId, count);
         return "redirect:/orders";
     }
 }
